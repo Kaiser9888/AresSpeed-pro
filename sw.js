4ac5925b80ad5b4f2534a21c4d5f6f11066e8b68
Kaiser9888 <kelvinlopes418@gmail.com>
2025-12-25 11:46:55 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Service Worker para Ares Speed
+const CACHE_NAME = 'ares-speed-v3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('📦 Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
